dd39665b7b23736943d1a9892b48d169f37b684c
Sai Panneerselvam <89541946+SaiPanneerselvam@users.noreply.github.com>
2022-05-20 08:58:00 -0400

    Create app.py

---
+++ build/python/app.py
@@ -0,0 +1,6 @@
+import j2t from Authors
+print('Welcome. Start pasting JavaScript lines to get the Tixel output in a list. Use command /f=Show() to reveal Tixel lines')
+while line=line:
+  line=input("")
+  if line=="var =":
+    
